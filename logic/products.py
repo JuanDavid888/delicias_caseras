3604692f24e35229edce56b1d603b8cb45233ef4
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-01-10 09:04:46 -0500

    Se empezo el proceso de ventas, agregar, editar y eliminar pedidos

--- logic/products.py
+++ logic/products.py
@@ -1,18 +1,25 @@
 from formula.products import updateQuantityInventory
 from tabulate import tabulate
 import json
+
 def findAll():
     with open("data/products.json", "r", encoding="utf-8") as file :
         data = file.read()
-        converted = json.loads(data)
-        return converted
+        convertedList = json.loads(data)
+        return convertedList
+    
 def saveAll(data):
     with open("data/products.json", "w", encoding="utf-8") as file:
         str(data).encode('utf-8')
         convertJSON = json.dumps(data, indent=4, ensure_ascii=False)
         file.write(convertJSON)
         return "Se modifico el archivo products.json"
-        
+
+def addProduct(formulary):
+    data = findAll()
+    data.append(formulary)
+    return saveAll(data)
+
 def updateInventoryByCode(code_product):
     data = findAll()
     for product in data:
@@ -22,28 +29,3 @@ def updateInventoryByCode(code_product):
             product.update({"cantidad_en_stock": stock})
             print(f"Se actualizo el stock de {code_product} a {stock}")
     print(saveAll(data))
-
-def addProduct(data, new_product):
-    data.append(new_product)
-    return data
-
-def newProduct():
-    print("Añada la informacion del nuevo producto")
-    print(" ")
-    new_product = {}
-    new_product["codigo_producto"] = input("Codigo: ")
-    new_product["nombre"] = input("Nombre: ")
-    new_product["categoria"] = input("Categoria: ")
-    new_product["descripcion"] = input("Descripcion breve: ")
-    new_product["proveedor"] = input("Proveedor: ")
-    new_product["cantidad_en_stock"] = int(input("Stock: "))
-    new_product["precio_venta"] = float(input("Precio de venta: "))
-    new_product["precio_proveedor"] = float(input("Precio de compra: "))
-
-    data = findAll()
-    data = addProduct(data, new_product)
-    saveAll(data)
-    print(" ")
-    print("se añadio el nuevo producto correctamente")
-       
-
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-from design.products import desing, tableProducts, tableProductsByCategory
-from logic.products import updateInventoryByCode, newProduct
+from design.products import desing as desingProducts, tableProducts, tableProductsByCategory, formularyAddProduct
+from logic.products import updateInventoryByCode
+from  design.customer import desing as desingCustomer, formularyTakeOrder
 
-match desing():
+match desingCustomer():
     case 0:
         print("¡Hasta luego!")
     case 1:
-        tableProducts()
-    case 2:
-        tableProductsByCategory(input("Ingrese la categoria a buscar, ejemplo (Panes, Pastel, Postres): "))
-    case 3:
-        updateInventoryByCode(input("Ingrese el codigo de producto, ejemplo (PN-001): "))
-    case 4: 
-        newProduct()
+        formularyTakeOrder()
     case _:
         print("Esa opcion no existe")
+
+#match desingProducts():
+    #case 0:
+        #print("¡Hasta luego!")
+    #case 1:
+        #tableProducts()
+    #case 2:
+        #tableProductsByCategory(input("Ingrese la categoria a buscar, ejemplo (Panes, Pastel, Postres): "))
+    #case 5:
+        #updateInventoryByCode(input("Ingrese el codigo de producto, ejemplo (PN-001): "))
+    #case 6: 
+        #formularyAddProduct()
+    #case _:
+        #print("Esa opcion no existe")
